73ccd430d4795e40217b1211393c7758895ef2a8
Dalyn Baldwin <dlbaldwi@byu.edu>
2022-12-08 12:54:00 -0700

    static files added

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 DISABLE_COLLECTSTATIC = 0
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'restaurants/static/')
